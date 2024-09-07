e4558abaf2d0b8627119d0d80eca03c1293056e0
Andrew Sukhanov <sukhanov.a.s22@yandex.ru>
2024-09-07 15:45:37 +0300

    new main func

--- roomBooking/roomBooking/src/main/java/com/conferenceroombooking/Main.java
+++ roomBooking/roomBooking/src/main/java/com/conferenceroombooking/Main.java
@@ -5,6 +5,7 @@ import roomBooking.roomBooking.src.main.java.com.conferenceroombooking.models.Ro
 import roomBooking.roomBooking.src.main.java.com.conferenceroombooking.models.User;
 import roomBooking.roomBooking.src.main.java.com.conferenceroombooking.services.BookingService;
 
+import java.awt.print.Book;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -35,8 +36,46 @@ public class Main {
             scanner.nextLine();
 
             switch (choice) {
-                case 1 -> {}
-                case 2 -> {}
+                case 1 -> {
+                    System.out.println("Adding new user");
+                    System.out.println("Enter name: ");
+                    String userName = scanner.nextLine();
+
+                    System.out.println("Enter surname: ");
+                    String userSurname = scanner.nextLine();
+
+                    users.add(new User(userName, userSurname));
+                    System.out.println(users);
+                }
+                case 2 -> {
+                    System.out.println("Adding new booking");
+
+                    System.out.print("Enter start time (dd-MM-yyyy HH:mm): ");
+                    String startTimeString = scanner.nextLine();
+                    System.out.print("Enter end time (dd-MM-yyyy HH:mm): ");
+                    String endTimeString = scanner.nextLine();
+
+                    LocalDateTime startTime = LocalDateTime.parse(startTimeString, formatter);
+                    LocalDateTime endTime = LocalDateTime.parse(endTimeString, formatter);
+
+                    System.out.println("Enter user: ");
+                    System.out.println("Enter name: ");
+                    String userName = scanner.nextLine();
+
+                    System.out.println("Enter surname: ");
+                    String userSurname = scanner.nextLine();
+
+                    User user = new User(userName, userSurname);
+                    if (!users.contains(user)) {
+                        users.add(user);
+                    }
+
+                    Booking booking = new Booking(startTime, endTime, user);
+
+                    bookings.add(booking);
+
+                    System.out.println(bookings);
+                }
                 case 3 -> {}
                 case 4 -> {
                     System.out.println("Exiting");
